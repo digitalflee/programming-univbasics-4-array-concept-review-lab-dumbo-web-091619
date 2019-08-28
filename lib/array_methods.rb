05c16ef0daf37d97687a2b3b5d688b28bf522783
Keenan Alves <keenan.alves@gmail.com>
2019-08-28 23:31:00 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -2,7 +2,7 @@
 def find_element_index(array, value_to_find)
  
 array.length.times do |index|
-  if array[index] == 
+  if array[index] == value_to_find
   puts array   
   end
   return array[value_to_find]
