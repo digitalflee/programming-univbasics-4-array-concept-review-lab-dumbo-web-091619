d6b5406814de8025eb2a273115e9aeef24d4f5da
Keenan Alves <keenan.alves@gmail.com>
2019-08-28 22:22:10 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -11,9 +11,21 @@ end
 
 
 def find_max_value(array)
+ array = []
+ length = array.length 
  
+length.times do |array|
+  puts array 
+  end
+  return array.max 
 end 
 
 def find_min_value(array)
+ array = []
+ length = array.length 
  
+length.times do |array|
+  puts array 
+  end
+  return array.min 
 end
